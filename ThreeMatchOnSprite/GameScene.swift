ca7247408cdcacf1e432fe72e5cf6e1b9e14a1be
lsdimagine <linshidongbqwl@gmail.com>
2015-09-19 17:12:28 -0700

    add first background

--- ThreeMatchOnSprite/GameScene.swift
+++ ThreeMatchOnSprite/GameScene.swift
@@ -6,37 +6,21 @@
 //  Copyright (c) 2015 Shidong Lin. All rights reserved.
 //
 
+import UIKit
 import SpriteKit
 
 class GameScene: SKScene {
-    override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 45;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
-        
-        self.addChild(myLabel)
-    }
     
-    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-       /* Called when a touch begins */
+    override init(size: CGSize) {
+        super.init(size: size)
         
-        for touch in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
-        }
+        anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        let background = SKSpriteNode(imageNamed: "Background")
+        addChild(background)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
    
     override func update(currentTime: CFTimeInterval) {
--- ThreeMatchOnSprite/GameViewController.swift
+++ ThreeMatchOnSprite/GameViewController.swift
@@ -10,24 +10,24 @@ import UIKit
 import SpriteKit
 
 class GameViewController: UIViewController {
+    var scene: GameScene!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        if let scene = GameScene(fileNamed:"GameScene") {
-            // Configure the view.
-            let skView = self.view as! SKView
-            skView.showsFPS = true
-            skView.showsNodeCount = true
+        // Configure the view.
+        let skView = self.view as! SKView
+        skView.showsFPS = true
+        skView.showsNodeCount = true
             
-            /* Sprite Kit applies additional optimizations to improve rendering performance */
-            skView.ignoresSiblingOrder = true
+        /* Sprite Kit applies additional optimizations to improve rendering performance */
+        skView.ignoresSiblingOrder = true
             
-            /* Set the scale mode to scale to fit the window */
-            scene.scaleMode = .AspectFill
+        /* Set the scale mode to scale to fit the window */
+        scene = GameScene(size: skView.bounds.size)
+        scene.scaleMode = .AspectFill
             
-            skView.presentScene(scene)
-        }
+        skView.presentScene(scene)
     }
 
     override func shouldAutorotate() -> Bool {
